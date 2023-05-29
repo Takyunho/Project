c1039a5e08dad3e30bd52e84727926732e668b9f
Takyunho <tyh1819@gmail.com>
2023-05-29 17:22:06 +0900

    Add: Logo 추가 (구글폰트 이용)

--- 2023/movie/src/components/Header.vue
+++ 2023/movie/src/components/Header.vue
@@ -1,16 +1,9 @@
 <template>
   <header>
+    <Logo></Logo>
     <div class="nav nav-pills">
-      <div 
-        class="nav-item" 
-        v-for="nav in navigations" 
-        :key="nav"
-      >
-        <RouterLink 
-          class="nav-link" 
-          :to="nav.href"
-          active-class="active"
-        >
+      <div class="nav-item" v-for="nav in navigations" :key="nav">
+        <RouterLink class="nav-link" :to="nav.href" active-class="active">
           {{ nav.name }}
         </RouterLink>
       </div>
@@ -20,29 +13,45 @@
 </template>
 
 <script>
-  export default {
+import Logo from '../components/Logo.vue'
+
+export default {
   data() {
-      return {
-        //- 경로를 효율적으로 관리하기 위해 navigation이라는 배열을 만들어서 관리한다.
-        navigations: [
-          {
-            name: 'Search',
-            href: '/'
-          },
-          {
-            name: 'Movie',
-            href: '/movie'
-          },
-          {
-            name: 'About',
-            href: '/about'
-          },
-        ]
-      }
+    return {
+      //- 경로를 효율적으로 관리하기 위해 navigation이라는 배열을 만들어서 관리한다.
+      navigations: [
+        {
+          name: 'Search',
+          href: '/'
+        },
+        {
+          name: 'Movie',
+          href: '/movie'
+        },
+        {
+          name: 'About',
+          href: '/about'
+        },
+      ]
     }
+  },
+  components: {
+    Logo
   }
+
+}
 </script>
 
 <style lang="scss" scoped>
-
+  header {
+    height: 70px;
+    padding: 0 40px;
+    display: flex;
+    align-items: center;
+    
+    // header 안에 들어있는 logo인 경우에만 스타일 적용됨
+    .logo {
+      margin-right: 40px;
+    }
+  }
 </style>
---
+++ 2023/movie/src/components/Logo.vue
@@ -0,0 +1,27 @@
+<template>
+  <RouterLink to="/" class="logo">
+    <span>Movie</span> App
+  </RouterLink>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/main.scss";
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black;  //bootstrap에 정의된 변수
+  text-decoration: none;
+  &:hover {
+    color: $black;
+  }
+  span {
+    color: $primary;
+  }
+}
+</style>
