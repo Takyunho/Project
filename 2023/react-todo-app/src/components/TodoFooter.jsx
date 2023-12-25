77a15647436bf0ba6334cb68f746f13f249134b6
Takyunho <tyh1819@gmail.com>
2023-12-25 12:56:24 +0900

    Feat: 할 일 추가하는 기능 구현
    - 상위 컴포넌트에서 set하고 하위 컴포넌트에서는 더하는 기능만 구현

--- 2023/react-todo-app/src/components/TodoFooter.jsx
+++ 2023/react-todo-app/src/components/TodoFooter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import styles from "./TodoFooter.module.css";
 
-export default function TodoFooter({ todoList, setTodoList }) {
+export default function TodoFooter({ todoList, onAdd }) {
   // 새로운 할 일(input 값)
   const [newTodo, setNewTodo] = useState("");
 
@@ -12,35 +13,45 @@ export default function TodoFooter({ todoList, setTodoList }) {
 
   // todoList에 새로운 할 일 추가
   const addTodo = () => {
-    setTodoList([
-      ...todoList,
-      {
-        id: todoList.length + 1,
-        content: newTodo,
-        status: "disabled",
-      },
-    ]);
-    setNewTodo(""); // input 초기화 
+    // newTodo가 빈 문자열이면 추가하지 않음
+    if (newTodo.trim() === "") {
+      return;
+    }
+
+    const todo = {
+      id: todoList.length + 1,
+      content: newTodo,
+      status: "disabled",
+    };
+
+    onAdd(todo);
+    setNewTodo(""); // input 초기화
   };
 
   const handleKeyDown = (e) => {
+    // console.log(e.key);
+    // console.log(e.keyCode);
     if (e.key === "Enter") {
+      if (e.nativeEvent.isComposing) return;  // 한글 입력시 이벤트가 두번 호출되는 현상 막기
       addTodo();
     }
   };
 
   return (
     <div>
-      <div>
+      <div className={styles.footer_wrap}>
         <input
           type="text"
           placeholder="할일을 입력하세요"
+          className={styles.input}
           value={newTodo}
           onChange={handleChange}
           onKeyDown={handleKeyDown}
         />
         {/* addTodo */}
-        <button onClick={addTodo}>Add</button>
+        <button className={styles.add_btn} onClick={addTodo}>
+          Add
+        </button>
       </div>
     </div>
   );
