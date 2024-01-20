25ccd6ea09ee32e23905cf4d4867b2a71512babe
Takyunho <tyh1819@gmail.com>
2024-01-20 22:51:02 +0900

    Feat: filter 기능 구현
    - all, active, completed를 나눠서 필터링 된 리스트만 보여주기

--- 2023/react-todo-app/src/components/TodoHeader.jsx
+++ 2023/react-todo-app/src/components/TodoHeader.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './TodoHeader.module.css';
 import { IoSunny } from "react-icons/io5";
 
-export default function TodoHeader() {
+export default function TodoHeader({ filters, filter, onFilterChange}) {
   return (
-    <div className={styles.container}>
+    <header className={styles.container}>
       {/* 다크모드 버튼 */}
       <button className={styles.mode_btn}>
         <span>
@@ -12,7 +12,12 @@ export default function TodoHeader() {
         </span>
       </button>
       {/* 전체보기 / 할 일 / 완료 */}
-    </div>
+      <ul>
+        {filters.map((value, index) => <li key={index}>
+          <button onClick={() => onFilterChange(value)}>{value}</button>
+        </li>)}
+      </ul>
+    </header>
   );
 }
 
