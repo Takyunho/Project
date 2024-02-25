e024dcc92fd8940c73385bba8c7aab1322fd9c21
Takyunho <tyh1819@gmail.com>
2024-02-25 19:35:03 +0900

    Feat: darkmode context 구현

--- 2023/react-todo-app/src/AppTodo.jsx
+++ 2023/react-todo-app/src/AppTodo.jsx
@@ -1,11 +1,14 @@
 import styles from './AppTodo.module.css';
 import TodoBox from './components/TodoBox';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function AppTodo() {
   return (
-    <div className={styles.background}>
-      <TodoBox />
-    </div>
+    <DarkModeProvider>
+      <div className={styles.background}>
+        <TodoBox />
+      </div>
+    </DarkModeProvider>
   )
 }
 
--- 2023/react-todo-app/src/components/TodoHeader.jsx
+++ 2023/react-todo-app/src/components/TodoHeader.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import styles from './TodoHeader.module.css';
-import { IoSunny } from "react-icons/io5";
+import { IoMoon, IoSunny } from "react-icons/io5";
+import { useDarkMode } from '../context/DarkModeContext';
 
-export default function TodoHeader({ filters, filter, onFilterChange}) {
+export default function TodoHeader({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.container}>
       {/* 다크모드 버튼 */}
-      <button className={styles.mode_btn}>
-        <span>
-          <IoSunny></IoSunny>
-        </span>
+      <button className={styles.mode_btn} onClick={toggleDarkMode}>
+        {/* darkmode가 아니라면 */}
+        {!darkMode && <IoMoon></IoMoon>}
+        {darkMode && <IoSunny></IoSunny>}
       </button>
       {/* 전체보기 / 할 일 / 완료 */}
       <ul className={styles.list_wrap}>
-        {filters.map((value, index) => <li key={index}>
-          <button className={styles.filter_btn} onClick={() => onFilterChange(value)}>{value}</button>
-        </li>)}
+        {filters.map((value, index) => (
+          <li key={index}>
+            <button
+              className={styles.filter_btn}
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
+          </li>
+        ))}
       </ul>
     </header>
   );
