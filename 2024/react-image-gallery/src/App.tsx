fd277f2c46b8e6c63afe27a53f09612d3e738b7f
Yunho <tyh1819@gmail.com>
2024-04-03 17:07:04 +0900

    Fix: file upload를 drag&drop 가능하도록 수정
    - react-dropzone 라이브러리 이용

--- 2024/react-image-gallery/src/App.tsx
+++ 2024/react-image-gallery/src/App.tsx
@@ -1,47 +1,57 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import ImageBox from "./components/ImageBox";
+import { useDropzone } from "react-dropzone"; // react dropzone
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [imageList, setImageList] = useState<string[]>([]);
-  console.log(imageList);
 
-  const fileUpload = () => {
-    // if (inputRef.current) {
-    //   inputRef.current.click()
-    // }
-    //- 위의 코드 축약
-    inputRef.current?.click();
-  };
+  // react dropzone (drop시 실행)
+  const onDrop = useCallback((acceptedFiles: any) => {
+    console.log(acceptedFiles);
 
-  const getFile = (event: any) => {
-    // console.log(event.target.value)
-    // console.log(event.currentTarget.value)
-
-    if (event.currentTarget.files?.[0]) {
-      // 0번째 파일이 있다면
-      const file = event.currentTarget.files[0];
-      console.log("%c 가져온 파일 객체", "color: lightblue;", file);
-
-      // file path를 가져오기 위해 FileReader를 사용
-      const reader = new FileReader();
-      console.log("%c reader", "color: lightgreen;", reader);
-      // file객체를 읽은 후 data url로 바꾸기
-      reader.readAsDataURL(file);
-      // load가 끝났으면
-      reader.onloadend = (e) => {
-        console.log("%c load가 끝난 후 event", "color: #78beff;", e);
-        console.log("%c event.target", "color: #78beff;", e.target);
-        setImageList((prevImg) => [...prevImg, e.target?.result as string]); // result는 여러개를 가질 수 있지만 data url이므로 무조건 string으로 들어오는 것을 알 수 있기 때문에 as string을 사용
-      };
+    if (acceptedFiles.length) {
+      for (const file of acceptedFiles) {
+        // file path를 가져오기 위해 FileReader를 사용
+        const reader = new FileReader();
+        // file객체를 읽은 후 data url로 바꾸기
+        reader.readAsDataURL(file);
+        // load가 끝났으면
+        reader.onloadend = (e) => {
+          console.log(e)
+          setImageList((prevImg) => [...prevImg, e.target?.result as string]); 
+        };
+      }
     }
-  };
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
-  // useEffect(() => {
-  //   console.log(imageList)
-  // }, [imageList]);
+  // const fileUpload = () => {
+  //   // button 클릭시 input을 클릭한 것 처럼하기
+  //   inputRef.current?.click();
+  // };
+
+  // const getFile = (event: any) => {
+  //   if (event.currentTarget.files?.[0]) {
+  //     // 0번째 파일이 있다면
+  //     const file = event.currentTarget.files[0];
+  //     console.log("%c 가져온 파일 객체", "color: lightblue;", file);
+
+  //     // file path를 가져오기 위해 FileReader를 사용
+  //     const reader = new FileReader();
+  //     console.log("%c reader", "color: lightgreen;", reader);
+  //     // file객체를 읽은 후 data url로 바꾸기
+  //     reader.readAsDataURL(file);
+  //     // load가 끝났으면
+  //     reader.onloadend = (e) => {
+  //       console.log("%c load가 끝난 후 event", "color: #78beff;", e);
+  //       console.log("%c event.target", "color: #78beff;", e.target);
+  //       setImageList((prevImg) => [...prevImg, e.target?.result as string]); // result는 여러개를 가질 수 있지만 data url이므로 무조건 string으로 들어오는 것을 알 수 있기 때문에 as string을 사용
+  //     };
+  //   }
+  // };
+
 
   return (
     <div className="container">
@@ -56,13 +66,18 @@ function App() {
         {imageList.map((imagePath, index) => (
           <ImageBox key={index} src={imagePath} />
         ))}
-        <input
-          type="file"
-          ref={inputRef}
-          style={{ display: "none" }}
-          onChange={getFile}
-        />
-        <button className="plus-box" onClick={fileUpload}>
+        <button
+          className="plus-box"
+          // onClick={fileUpload}
+          {...getRootProps()}>
+          
+          <input
+            type="file"
+            ref={inputRef}
+            style={{ display: "none" }}
+            // onChange={getFile}
+            {...getInputProps()}
+          />
           +
         </button>
       </div>
