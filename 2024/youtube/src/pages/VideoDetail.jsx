f61d5d7678648e5544ca8c1e8f935d7066fc1478
Yunho <tyh1819@gmail.com>
2024-05-02 21:22:06 +0900

    Add: page에 관련된 컴포넌트 생성

---
+++ 2024/youtube/src/pages/VideoDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function VideoDetail() {
+  return (
+    <div>
+      video detail
+    </div>
+  );
+}
+
---
+++ 2024/youtube/src/pages/Videos.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Videos() {
+  return (
+    <div>
+      Videos
+    </div>
+  );
+}
+
