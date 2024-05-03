553763f66a4fb6b2570e23f9b3daf9e92142cdfc
Yunho <tyh1819@gmail.com>
2024-05-03 14:31:14 +0900

    Feat: 검색 헤더 스타일링

--- 2024/youtube/src/components/SearchHeader.jsx
+++ 2024/youtube/src/components/SearchHeader.jsx
@@ -24,14 +24,14 @@ export default function SearchHeader() {
   }, [keyword])
   
   return (
-    <header>
-      <Link to='/'>
-        <BsYoutube />
-        <h1>Youtube</h1>
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to='/' className='flex items-center mr-2'>
+        <BsYoutube className='text-4xl text-brand'/>
+        <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Search...' value={text} onChange={handleSearch} />
-        <button>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
+        <input className='w-7/12 p-2 outline-none bg-black text-gray-50' type="text" placeholder='Search...' value={text} onChange={handleSearch} />
+        <button className='bg-zinc-600 px-4'>
           <BsSearch />
         </button>
       </form>
--- 2024/youtube/tailwind.config.js
+++ 2024/youtube/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      //! 내가 원하는 색상 지정가능
+      colors: {
+        brand: '#FF0000', // 브랜드 색상 지정하기
+      }
+    },
   },
   plugins: [],
 };
