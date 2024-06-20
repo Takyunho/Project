99ba57902265e71e8d585f96456ffab6d2bc73d1
Yunho <tyh1819@gmail.com>
2024-06-20 20:22:32 +0900

    Feat: 비디오 디테일 페이지 구현
    - useLocation으로 VideoCard에서 navigate 될 떄 전달된 state 값을 받음
    - 전달받은 video 객체를 이용하여 title과 같은 정보들을 바인딩
    - iframe을 이용해서 유튜브 영상을 보여줌
    - 채널에 대한 정보를 표시할 ChannelInfo 컴포넌트와 연관된 비디오를 보여줄 RelatedVideos 컴포너트를 구현했고, props로 필요한 정보를 보냄

---
+++ 2024/youtube/src/components/ChannelInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ChannelInfo({ id, name }) {
+  console.log(id, name)
+
+  return (
+    <div>
+      {name}
+    </div>
+  );
+}
+
--- 2024/youtube/src/pages/VideoDetail.jsx
+++ 2024/youtube/src/pages/VideoDetail.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 export default function VideoDetail() {
+  //* VideoCard에서 넘겨준 state를 받아서 사용하기
+  const { state: { video } } = useLocation();
+  console.log(video)
+  const { title, channelId, channelTitle, description } = video.snippet;
+
+  // 채널 썸네일 받아오기
+
   return (
-    <div>
-      video detail
-    </div>
+    <section>
+      <article>
+        <iframe
+          id="player"
+          type="text/html"
+          width="100%"
+          height="640"
+          src={`http://www.youtube.com/embed/${video.id}`}
+          title={video.snippet.title}
+          // frameBorder="0"  // HTML5에서는 iframe의 frameBorder속성값은 더이상 지원하지 않는다고 한다
+        ></iframe>
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle}></ChannelInfo>
+          {/* pre 태그를 이용해서 여백이나 공백, 스페이스, 엔터가 적절히 렌더링 되도록 한다. */}
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
   );
 }
 
